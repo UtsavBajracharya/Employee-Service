ea9986a93beb8098350d5caec559b0baa51d3de5
Utsav <utsavbajra619@gmail.com>
2024-11-27 05:24:24 -0500

    add models route in server

--- app.js
+++ app.js
@@ -3,50 +3,28 @@ const bodyParser = require('body-parser');
 const cors = require('cors');
 const path = require('path');
 const employeeRoutes = require('./routes/employeeRoutes');
+const EmployeeModel = require('./models/employeeModel');
 
 const app = express();
 
+// Middleware
 app.use(cors());
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
-app.set('view engine', 'ejs');
-app.set('views', path.join(__dirname, 'views'));
 app.use(express.static(path.join(__dirname, 'public')));
 
-// In-memory JSON array for employee data
-const employees = [];
+// View Engine
+app.set('view engine', 'ejs');
+app.set('views', path.join(__dirname, 'views'));
 
 // Routes
 app.use('/api', employeeRoutes);
 
-// Tell Express where to find the views
-app.set('views', path.join(__dirname, 'views'));
-
 // UI Endpoint
 app.get('/', (req, res) => {
-    res.render('index', { employees }); // Pass employee data to the view
+    res.render('index', { employees: EmployeeModel.getAllEmployees() }); // Pass employee data to the view
 });
 
-app.post('/add-employee', (req, res) => {
-    const { firstName, lastName, id, salary, department, email } = req.body;
-  
-    // Check if employee exists
-    const existingEmployee = employees.find((emp) => emp.id === id);
-  
-    if (existingEmployee) {
-      // Update existing employee
-      existingEmployee.firstName = firstName;
-      existingEmployee.lastName = lastName;
-      existingEmployee.salary = salary;
-      existingEmployee.department = department;
-      existingEmployee.email = email;
-    } else {
-      // Add new employee
-      employees.push({ firstName, lastName, id, salary, department, email });
-    }
-  
-    res.redirect('/'); // Reload the page after adding/updating
-  });
 
 // Add port
 const PORT = 3000;
